e1e03b7a66445b1bafb16d20160cfdb4186be93f
reece-elder <reece.elder@qa.com>
2021-10-06 09:53:32 +0100

    basic return methods added

---
+++ ReturnTypes/src/com/qa/demo/ReturnTypes.java
@@ -0,0 +1,24 @@
+package com.qa.demo;
+
+public class ReturnTypes {
+	
+	// Primitives - Simpler, single value things 5, a, true, 0
+	// Complex - References to multiple primitive data types, such as "Hello everyone"
+
+	// Returns - Methods within java should return SOMETHING 
+	
+	// All this method does is return 36
+	// returnInt() = 36
+	// Explicitly saying this method returns an int
+	public static int returnInt() {
+		return 36;
+	}
+	
+	// How to create a method that returns a String
+	public static String returnString() {
+		return "Hey, this is being returned :o";
+	}
+	
+	
+	
+}
--- ReturnTypes/src/com/qa/demo/Runner.java
+++ ReturnTypes/src/com/qa/demo/Runner.java
@@ -1,5 +1,27 @@
 package com.qa.demo;
 
 public class Runner {
+	
+	public static void main(String[] args) {
+		boo();
+		helloWorld();
+		// Im accessing the class (ReturnTypes).method
+//		ReturnTypes.returnInt();
+		System.out.println(ReturnTypes.returnInt());
+		System.out.println(ReturnTypes.returnString());
+	}
+	
+	// Creating a method that prints hello world
+	// getting our main to run that method
+	
+	// This is what we'd put in MAIN
+	
+	public static void helloWorld() {
+		System.out.println("Hello World");
+	}
+	
+	public static void boo() {
+		System.out.println("spooooooooky");
+	}
 
 }
