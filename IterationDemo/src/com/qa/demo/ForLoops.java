373b3a943576f4ca4088eb0e856661bd8aecb4e3
reece-elder <reece.elder@qa.com>
2021-10-07 14:41:47 +0100

    for loops

---
+++ IterationDemo/src/com/qa/demo/ForLoops.java
@@ -0,0 +1,48 @@
+package com.qa.demo;
+
+public class ForLoops {
+	
+	// For Loops are for when you know how long the loop should go on FOR
+	
+	// They have four distinct components to make up
+	// Initialisation (initial value)
+	// Condition (What it checks for)
+	// Iterator (How the value changes)
+	// Code block (what do you want to loop)
+	
+	public static void loopDemo() {
+		
+		// Create a loop that counts to 10 
+		
+		// Key word for for
+		for (
+				int i = 0; // Initialisation (what are we starting from)
+				i <= 10;    // Condition (Why does the loop stop)
+				i++        // How does i change
+			) {
+			// What is the loop doing
+			System.out.println("Value of i is :" + i);
+		}
+		
+		// Create a loop that counts from 10 down to 1
+		for(int x = 10; x > 0; x--) {
+			System.out.println("Value of x is : " + x);
+		}
+		
+		// Create a loop that counts in 5s from 20 to 100
+		// ++ - adds 1 
+		// -- - subtracts 1
+		// += 123 adds 123
+		// y = y + 123  //  y += 123
+		
+		for(int y = 20; y < 100; y += 5) {
+			System.out.println("Value of Y is :" + y);
+			
+		}
+		
+		
+	}
+	
+	
+
+}
--- IterationDemo/src/com/qa/runner/Runner.java
+++ IterationDemo/src/com/qa/runner/Runner.java
@@ -1,13 +1,15 @@
 package com.qa.runner;
 
+import com.qa.demo.ForLoops;
 import com.qa.demo.IterationMethods;
 
 public class Runner {
 	
 	public static void main(String[] args) {
 		
-		IterationMethods.whileDemo();
-		IterationMethods.doWhileDemo();
+//		IterationMethods.whileDemo();
+//		IterationMethods.doWhileDemo();
+		ForLoops.loopDemo();
 		
 	}
 
