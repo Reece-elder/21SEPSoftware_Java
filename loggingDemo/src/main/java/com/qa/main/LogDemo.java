55a7c8aa4d91340fba07b679b44918b04586864c
reece-elder <reece.elder@qa.com>
2021-10-14 16:25:27 +0100

    log4j added

---
+++ loggingDemo/src/main/java/com/qa/main/LogDemo.java
@@ -0,0 +1,21 @@
+package com.qa.main;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+// import org.apache.logging.log4j.*; <-- gets everything from org.apache.logging.log4j
+
+public class LogDemo {
+	
+	// Logging is the process of outputting data from the computer to the console
+	// System.out.println("some data") <-- Logging 
+	// log4j is a third party 'better' log tool
+	
+	// First thing to do is create a Logger object 
+
+	public static Logger newLogger = LogManager.getLogger();
+	
+	public static void logTest(String name) {
+		newLogger.fatal("Hey " + name);
+	}
+
+}
---
+++ loggingDemo/src/main/java/com/qa/main/Runner.java
@@ -0,0 +1,12 @@
+package com.qa.main;
+
+public class Runner {
+	
+
+	
+	public static void main(String[] args) {
+		System.out.println("Not using anymore");
+		LogDemo.logTest("Fraser");
+	}
+
+}
