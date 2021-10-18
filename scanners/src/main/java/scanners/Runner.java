8ecfcdd3907f4bbe9450ddb03f5970e02516513e
Jordan <jordanbenbelaid@hotmail.com>
2021-10-18 14:21:20 +0100

    Basic scanner intro

---
+++ scanners/src/main/java/scanners/Runner.java
@@ -0,0 +1,14 @@
+package scanners;
+
+import java.util.Scanner;
+
+public class Runner {
+	
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("What football team do you support?");
+		String team = scanner.nextLine();
+	}
+
+}
